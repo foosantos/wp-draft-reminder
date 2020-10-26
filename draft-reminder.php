6c454ff93198e4dc9139499f4bbea5391df8b910
Felipe Santos <info@felipels.com>
2020-10-26 23:35:27 +0000

    Initial setup

--- draft-reminder.php
+++ draft-reminder.php
@@ -1,16 +1,28 @@
 <?php
 /**
  * Plugin Name:     Draft Reminder
- * Plugin URI:      PLUGIN SITE HERE
  * Description:     Draft Reminder notifications for WordPress.
  * Author:          Felipe Santos
- * Author URI:      YOUR SITE HERE
  * Text Domain:     draft-reminder
  * Domain Path:     /languages
  * Version:         0.1.0
  *
  * @package         Draft_Reminder
  */
+
+add_action( 'draftreminder_sender', 'draftreminder_send_email' );
+
+function draftreminder_activation() {
+    wp_schedule_event( time(), 'weekly', 'draftreminder_sender' );
+}
+
+register_activation_hook( __FILE__, 'draftreminder_activation' );
+
+function draftreminder_deactivation() {
+    wp_clear_scheduled_hook( 'draftreminder_sender' );
+}
+
+register_deactivation_hook( __FILE__, 'draftreminder_deactivation' );
 
 function draftreminder_get_drafts() {
 
@@ -43,15 +55,27 @@ function draftreminder_get_drafts() {
 	return $drafts_data;
 }
 
+function draftreminder_send_email() {
 
+	$drafts = draftreminder_get_drafts();
+	$email_subject = 'The subject';
+	$email_headers = array('Content-Type: text/html; charset=UTF-8');
 
+	foreach ($drafts as $author_id => $posts) {
 
+		$email_to = get_the_author_meta( 'user_email', $author_id );
+		$email_body = 'Hey there,<br><br>';
+		$email_body .= '<ul>';
 
+		foreach ($posts as $post_id => $post_meta) {
 
+			$email_body .= '<li>' . $post_meta['title'] . '</li>';
 
+		}
 
-function testing() {
-	var_dump(draftreminder_get_drafts());
+		$email_body .= '</ul>';
+
+		wp_mail( $email_to, $email_subject, $email_body, $email_headers );
+
+	}
 }
-
-add_action( 'wp_footer' , 'testing' );
