e7424fa9e36d05539a2109f5302e0b0928cec5c2
Felipe Santos <info@felipels.com>
2020-12-01 20:25:53 +0000

    Update helpers

--- includes/helpers.php
+++ includes/helpers.php
@@ -7,12 +7,12 @@
  */
 function get_days_of_the_week() {
 	return [
-		'sunday' => __('Sunday'),
-		'monday' => __('Monday'),
-		'tuesday' => __('Tuesday'),
-		'wednesday' => __('Wednesday'),
-		'thursday' => __('Thursday'),
-		'friday' => __('Friday'),
-		'saturday' => __('Saturday')
+		'sunday' => esc_html__('Sunday', 'draft-reminder'),
+		'monday' => esc_html__('Monday', 'draft-reminder'),
+		'tuesday' => esc_html__('Tuesday', 'draft-reminder'),
+		'wednesday' => esc_html__('Wednesday', 'draft-reminder'),
+		'thursday' => esc_html__('Thursday', 'draft-reminder'),
+		'friday' => esc_html__('Friday', 'draft-reminder'),
+		'saturday' => esc_html__('Saturday', 'draft-reminder')
 	];
 }
